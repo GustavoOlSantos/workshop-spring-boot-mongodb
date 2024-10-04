ff997a39f707ca1b702436565054360a1b5f783b
GustavoOlSantos <santos.gustavo.of@gmail.com>
2024-10-04 11:06:20 -0300

    Criando Entity post com User desnormalizado

--- src/main/java/com/gustavo_profissional/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gustavo_profissional/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.gustavo_profissional.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.gustavo_profissional.workshopmongo.domain.Post;
 import com.gustavo_profissional.workshopmongo.domain.User;
+import com.gustavo_profissional.workshopmongo.repository.PostRepository;
 import com.gustavo_profissional.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,19 +18,30 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... arg0) throws Exception {
 		
+		SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/yyyy");
+		sfd.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		//=> Deleta do MongoDB
 		userRepository.deleteAll();	
+		postRepository.deleteAll();	
 		
 		User miles = new User(null, "Miles Morales", "miles@gmail.com");
 		User hobbie = new User(null, "Hobbie Brown", "hobbie@outlook.com");
 		User gwen = new User(null, "Gwen Stacy", "gwen@gmail.com");
 		
+		Post post1 = new Post(null, sfd.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Rio de Janeiro. Abraços!", miles);
+		Post post2 = new Post(null, sfd.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", miles);
+		
 		//=> Salva a lista no MongoDB (Salvar apenas um elemento, utilize o Save())
 		userRepository.saveAll(Arrays.asList(miles, hobbie, gwen));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
