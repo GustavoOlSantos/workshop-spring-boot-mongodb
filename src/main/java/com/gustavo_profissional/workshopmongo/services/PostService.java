2d28172ed5f153a712a8eec5a3ed1bea340fb55b
GustavoOlSantos <santos.gustavo.of@gmail.com>
2024-10-04 14:34:20 -0300

    Consulta com diversos critérios em @Query

--- src/main/java/com/gustavo_profissional/workshopmongo/services/PostService.java
+++ src/main/java/com/gustavo_profissional/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.gustavo_profissional.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,6 +37,13 @@ public class PostService {
 	public List<Post> findByTitle(String text){
 		//return repo.findByTitleContaining(text);
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		
+		//=> Adiciona +24h no maxDate para considerar as 24h da data informada
+		maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 
 }
