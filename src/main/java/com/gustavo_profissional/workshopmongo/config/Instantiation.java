43d8920ad8a67c4984ca0636a69fb53e30c65916
GustavoOlSantos <santos.gustavo.of@gmail.com>
2024-10-04 12:08:24 -0300

    Implementando Comentários aos posts

--- src/main/java/com/gustavo_profissional/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gustavo_profissional/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.gustavo_profissional.workshopmongo.domain.Post;
 import com.gustavo_profissional.workshopmongo.domain.User;
 import com.gustavo_profissional.workshopmongo.dto.AuthorDTO;
+import com.gustavo_profissional.workshopmongo.dto.CommentDTO;
 import com.gustavo_profissional.workshopmongo.repository.PostRepository;
 import com.gustavo_profissional.workshopmongo.repository.UserRepository;
 
@@ -40,8 +41,15 @@ public class Instantiation implements CommandLineRunner {
 		//=> Salva a lista no MongoDB (Salvar apenas um elemento, utilize o Save())
 		userRepository.saveAll(Arrays.asList(miles, hobbie, gwen));
 		
-		Post post1 = new Post(null, sfd.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Rio de Janeiro. Abraços!", new AuthorDTO(miles));
-		Post post2 = new Post(null, sfd.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(miles));
+		Post post1 = new Post(null, sfd.parse("21/09/2024"), "Partiu viagem", "Vou viajar para Rio de Janeiro. Abraços!", new AuthorDTO(miles));
+		Post post2 = new Post(null, sfd.parse("23/09/2024"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(miles));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem Mano!", sfd.parse("21/09/2024"), new AuthorDTO(hobbie));
+		CommentDTO c2 = new CommentDTO("Aproveite", sfd.parse("22/09/2024"), new AuthorDTO(gwen));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sfd.parse("23/09/2024"), new AuthorDTO(hobbie));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		//=> Salva a lista no MongoDB (Salvar apenas um elemento, utilize o Save())
 		postRepository.saveAll(Arrays.asList(post1, post2));
